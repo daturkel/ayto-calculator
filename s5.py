36867baa58d004577e78f9257270d6ab3a636667
Dan Turkel <daturkel@gmail.com>
2017-01-31 17:36:13 -0500

    initial season 5 data

---
+++ s5.py
@@ -0,0 +1,33 @@
+# list of the guys' names, in alphabetical order
+guys = ["Andre",    #0
+        "Derrick",  #1
+        "Edward",   #2
+        "Hayden",   #3 
+        "Jaylan",   #4
+        "Joey",     #5
+        "Michael",  #6
+        "Mike",     #7
+        "Osvaldo",  #8
+        "Ozzy",     #9
+        "Tyler"]    #10
+
+# list of the girls' names, in alphabetical order
+girls = ["Alicia",   #0
+         "Carolina", #1
+         "Cas",      #2
+         "Gianna",   #3
+         "Hannah",   #4
+         "Kam",      #5
+         "Kari",     #6
+         "Kathryn",  #7
+         "Shannon",  #8
+         "Taylor",   #9
+         "Tyranny",] #10
+
+# each truth booth revelation
+# [guy_index, girl_index, perfect_match?]
+truth_booth = [[3,3,False]]
+
+# each matching ceremony guess and number of resultant beams
+# [[guy_0_girl, guy_1_girl, ... guy_8_girl, guy_9_girl], number_correct]
+mc = [[[0,7,5,8,2,1,4,6,10,3,9],2]]
