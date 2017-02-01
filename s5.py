c986dc937d8eea21a09eb0cc3a93a01e3426524b
Dan Turkel <daturkel@gmail.com>
2017-01-31 23:39:29 -0500

    update s5 data

--- s5.py
+++ s5.py
@@ -26,8 +26,12 @@ girls = ["Alicia",   #0
 
 # each truth booth revelation
 # [guy_index, girl_index, perfect_match?]
-truth_booth = [[3,3,False]]
+truth_booth = [[3,3,False],
+               [0,0,False],
+               [9,1,False]]
 
 # each matching ceremony guess and number of resultant beams
 # [[guy_0_girl, guy_1_girl, ... guy_8_girl, guy_9_girl], number_correct]
-mc = [[[0,7,5,8,2,1,4,6,10,3,9],2]]
+mc = [[[0,7,5,8,2,1,4,6,10,3,9],2],
+      [[4,0,8,9,5,1,3,2,6,7,10],0],
+      [[6,4,5,1,2,7,9,0,10,3,8],4]]
